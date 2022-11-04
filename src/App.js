5ace2ba4b46fcac9292bb68dc6399ace33350df5
germanwyeworks <german@wyeworks.com>
2022-11-04 17:35:14 -0300

    add conversationAdded event

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import conversationJson from './resources/messagesMock.json';
 function App() {
   const conversationData = JSON.parse(JSON.stringify(conversationJson));
 
-  const [client, setClient] = useState();
+  //const [client, setClient] = useState();
   const [isLoading, setIsLoading] = useState(true)
   const [token, setToken] = useState("");
   const [conversationSelected, setConversationSelected] = useState(conversationData[0]);
@@ -20,22 +20,24 @@ function App() {
   const [conversationSelectedIndex, setConversationSelectedIndex] = useState(0);
 
 
-  localStorage.setItem("username", "Lucas");
+  localStorage.setItem("username", "German Alvarez");
   const userLogged = localStorage.getItem('username');
-  useEffect(() => {
-    fetch(`http://localhost:5000/auth/user/${userLogged}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const client = new Client(data.token);
-        setClient(client);
-        setToken(data.token);
-        setListOfMessages(getSubscribedConversations(client));
-        setIsLoading(false);
-      });
+  // useEffect(() => {
+  //   fetch(`http://localhost:5000/auth/user/${userLogged}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const client = new Client(data.token);
+  //       console.log(data);
+  //       setClient(client);
+  //       setToken(data.token);
+  //       //setListOfMessages(getSubscribedConversations(client));
+  //       getConversationsAdded(client);
+  //       setIsLoading(false);
+  //     });
 
-  }, []);
+  // }, []);
 
-
+  const client = new Client("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzQyNzE4ZDc2MDg4NzVlZjk4YzFlM2YwZjcwMmJkNWU1LTE2Njc1Nzk2NjUiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJHZXJtYW4gQWx2YXJleiIsImNoYXQiOnsic2VydmljZV9zaWQiOiJJUzc2NGU3MDM1OTRmYjRhNWY5ZjYzNjVhOTkzYWU3MzE4In19LCJpYXQiOjE2Njc1Nzk2NjUsImV4cCI6MTY2NzU4MzI2NSwiaXNzIjoiU0s0MjcxOGQ3NjA4ODc1ZWY5OGMxZTNmMGY3MDJiZDVlNSIsInN1YiI6IkFDM2QyZTRkNmQzZDI1ZTc1MWY2YWJjM2ZiMTZkZjBmNWQifQ.5eyBb5V43EGj6F7fbvH6OYOc169xXM9b5bPjIeKhIgM");
   const createNewConversation = (info) => {
     console.log(info);
     client.createConversation().then((data)=>{
@@ -76,6 +78,12 @@ function App() {
     })
   }
 
+
+
+  client.on("conversationAdded", (conversation) => {
+    getSubscribedConversations(client);
+  })
+  
   const getSubscribedConversations = async(
     client
   ) => {
@@ -86,11 +94,15 @@ function App() {
       subscribedConversations = await subscribedConversations.nextPage();
       conversations = [...conversations, ...subscribedConversations.items];
     }
-    
     console.log(conversations);
-    console.log(conversations[2].lastMessage);
+    conversations.forEach((element,index) => {
+      console.log("Index: " + index,"Date created: " + element.dateCreated);
+      console.log("Index: " + index, "Date updated: " + element.dateUpdated);
+    });
+    //console.log(conversations[2].lastMessage);
     return conversations;
   }
+  getSubscribedConversations(client);
 
   const handleConversationSelection = (evt) => {
     setConversationSelectedIndex(+evt.target.id);
